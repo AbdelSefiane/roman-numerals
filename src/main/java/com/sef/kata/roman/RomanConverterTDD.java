1342a978cbb683b0703b353f6d1ac2b15939352d
Droopy <abdel.sefiane@gmail.com>
2022-05-27 13:21:23 +0200

    3rd refacto

--- src/main/java/com/sef/kata/roman/RomanConverterTDD.java
+++ src/main/java/com/sef/kata/roman/RomanConverterTDD.java
@@ -9,12 +9,10 @@ public class RomanConverterTDD implements Converter {
     @Override
     public String intToString(int i) {
         LinkedList<String> output = new LinkedList<>();
-        if(i >= 3){
+        int rest = i;
+        while(rest-1>=0){
             output.push(I);
-        } if( i >= 2){
-            output.push(I);
-        } if(i >= 1){
-            output.push(I);
+            rest--;
         }
         return output.stream().reduce((String x,String y) -> x+y).get();
     }
