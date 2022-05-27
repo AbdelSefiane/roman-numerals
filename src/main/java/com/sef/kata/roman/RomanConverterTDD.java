25e68ab30fa8754bfd7e75d0a6c33deb9439efb7
Droopy <abdel.sefiane@gmail.com>
2022-05-27 12:50:56 +0200

    2nd refacto

--- src/main/java/com/sef/kata/roman/RomanConverterTDD.java
+++ src/main/java/com/sef/kata/roman/RomanConverterTDD.java
@@ -1,13 +1,15 @@
 package com.sef.kata.roman;
 
-public class RomanConverterTDD implements Converter{
+public class RomanConverterTDD implements Converter {
+
+    public static final String I = "I";
 
     @Override
     public String intToString(int i) {
-        if(1 == i){
-            return "I";
+        if (1 == i) {
+            return I;
         } else {
-            return "II";
+            return I + I;
         }
     }
 }
--- src/test/java/RomanNumeralsTDD.java
+++ src/test/java/RomanNumeralsTDD.java
@@ -6,13 +6,13 @@ import static org.junit.Assert.assertEquals;
 public class RomanNumeralsTDD {
 
     @Test
-    public void shouldConvert1ToI(){
+    public void shouldConvert1ToI() {
         RomanConverterTDD romanConverterTDD = new RomanConverterTDD();
-        assertEquals("I", romanConverterTDD.intToString(1));
+        assertEquals(RomanConverterTDD.I, romanConverterTDD.intToString(1));
     }
 
     @Test
-    public void shouldConvert2ToII(){
+    public void shouldConvert2ToII() {
         RomanConverterTDD romanConverterTDD = new RomanConverterTDD();
         assertEquals("II", romanConverterTDD.intToString(2));
     }
